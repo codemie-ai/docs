44d3e4c9e333c97b2ccdfed46faef6ecb6c1f131
Alexey Gurinovich <shagon1k@gmail.com>
2026-09-09 11:47:11 +0200

    docs(user-guide): document table copy-to-clipboard button (#342)
    Co-authored-by: Aliaksei Hurynovich <aliaksei_hurynovich@epam.com>

--- sidebars.ts
+++ sidebars.ts
@@ -69,6 +69,7 @@ const sidebars: SidebarsConfig = {
                 'user-guide/assistants/share-assistant-chat-with-other-users',
                 'user-guide/assistants/export-assistant-chat-messages-to-word-and-pdf-formats',
                 'user-guide/assistants/html-preview',
+                'user-guide/assistants/copy-table',
                 'user-guide/assistants/chat-input-settings',
               ],
             },
